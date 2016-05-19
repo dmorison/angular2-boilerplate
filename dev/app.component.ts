8f4ffb440990d0356d92bd98331788174c369699
David Morison <david.morison@moneysavingexpert.com>
2016-05-19 15:14:27 +0100

    different data binding examples

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,25 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <p>{{ 'Hello' }}</p>
+        <input type="text" [value]="name" (keyup)="onKeyUp(inputElement.value)" #inputElement>
+        <p>{{ values }}</p>
+        <input type="text" [value]="'place holder text'" (keyup)="0" #oneWayElement>
+        <p>{{ oneWayElement.value }}</p>
+        <input type="text" [(ngModel)]="name">
+        <p>Greetings: {{ name }}</p>
     `,
 })
 export class AppComponent {
+	name = 'Dave';
+	values = '';
+
+	onKeyUp(value: string) {
+		this.values += value + ' | ';
+	}
+
+	onTest() {
+		return false;
+	}
 
 }
